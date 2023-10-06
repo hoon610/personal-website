603401dca3039437984942507b3a33ed6a1d4542
hoon610 <123856266+hoon610@users.noreply.github.com>
2023-10-06 13:19:32 -0400

    Update linkedin links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
           <GitHubIcon />
         </a>
         <a
-          href="https://www.linkedin.com/in/hoon-kang-7b048a239/"
+          href="https://www.linkedin.com/in/hoon-kang-az"
           target="_blank"
           rel="noopener noreferrer"
         >
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
             passion for problem-solving
           </p>
           <a
-            href="https://www.linkedin.com/in/hoon-kang-7b048a239/"
+            href="https://www.linkedin.com/in/hoon-kang-az"
             target="_blank"
             rel="noopener noreferrer"
           >
