04e00f9372329ddb6c71cbe72252817e8dc304b0
hoon610 <123856266+hoon610@users.noreply.github.com>
2023-11-10 14:35:32 -0500

    Update Experience.js

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -7,6 +7,7 @@ import "react-vertical-timeline-component/style.min.css";
 import SchoolIcon from "@mui/icons-material/School";
 import "../styles/Experience.css";
 import BiotechIcon from "@mui/icons-material/Biotech";
+import ComputerIcon from "@mui/icons-material/Computer";
 
 function Experience() {
   return (
@@ -19,9 +20,9 @@ function Experience() {
           icon={<SchoolIcon></SchoolIcon>}
         >
           <h3 className="vertical-timeline-element-title">
-            Arizona State University
+            B.S. in Biological Sciences
           </h3>
-          <p>B.S. in Biological Sciences</p>
+          <p>Arizona State University</p>
           <p>
             Launched and maintained a digital database of physical and GIS-based
             cartographical items in the ASU Map and Geospatial Hub. Spear-headed
@@ -36,8 +37,9 @@ function Experience() {
           icon={<BiotechIcon></BiotechIcon>}
         >
           <h3 className="vertical-timeline-element-title">
-            Ugarova Laboratories
+            Research Scientist
           </h3>
+          <p>Ugarova Laboratories</p>
           <p>
             Launched an automated data pipeline for documenting and creating
             ELISA immunoassays. Engineered a novel method for synthesisizing
@@ -46,19 +48,35 @@ function Experience() {
         </VerticalTimelineElement>
         <VerticalTimelineElement
           className="vertical-timeline-element--education"
-          date="2020 - 2023"
+          date="2020 - Present"
           iconStyle={{ background: "#AAD2BA", color: "#1D1E18" }}
           icon={<SchoolIcon></SchoolIcon>}
         >
           <h3 className="vertical-timeline-element-title">
-            University of Massachussets Lowell
+            M.S. in Computational Biology
           </h3>
-          <p>M.S. in Computational Biology</p>
+          <p>University of Massachussets Lowell</p>
           <p>
             Published two peer-reviewed scientific papers on the nest
             architecture of subterranean ant nests. Collected raw data
             transformed it through a Python visualization library and analyzed
             it using a GLMM coded from R's lme4 package.
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2021 - Present"
+          iconStyle={{ background: "#AAD2BA", color: "#1D1E18" }}
+          icon={<ComputerIcon></ComputerIcon>}
+        >
+          <h3 className="vertical-timeline-element-title">Web Developer</h3>
+          <p>Freelance</p>
+          <p>
+            Utilized front-end and back-end technologies such as ReactJS, VueJS,
+            Nodes,js, and .NET to create responsive, and user-friendly websites
+            for local businesses. Managed end-to-end project lifecycle, from
+            initial client meetings and project scope definition to coding,
+            testing, and deployment.
           </p>
         </VerticalTimelineElement>
       </VerticalTimeline>
